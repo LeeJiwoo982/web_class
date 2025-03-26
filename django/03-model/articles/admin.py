994955b52313569eb455cdf9f845c231f6bef413
Leejiwoo <easywoo980902@gmail.com>
2025-03-26 11:05:04 +0900

    add 0326 study

--- django/03-model/articles/admin.py
+++ django/03-model/articles/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Article
+# 현재 폴더 안에 있는 models모듈에서 Article 클래스 가져오겠다 명시
 
 # Register your models here.
+admin.site.register(Article)    # 게시글 db
--- django/03-model/articles/models.py
+++ django/03-model/articles/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+# 모델에서 DB 테이블 만들기 실습
+# 게시글이 저장될 테이블 설계하는 클래스
+# 컬럼이름 등을 설정
+class Article(models.Model):    # 파스칼케이스로 이름짓기
+    title = models.CharField(max_length=10)                 # 게시글 제목
+    content = models.TextField()                            # 게시글 내용
+    created_at = models.DateTimeField(auto_now_add=True)    # 생성일
+    updated_at = models.DateTimeField(auto_now=True)        # 수정일
